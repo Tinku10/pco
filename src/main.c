86f027c3c0c213783c4c2c93c2b8f35926a679d8
Tinku Kalita <tinku.kvs@gmail.com>
2026-09-18 18:47:10 +0530

    C with command-line flags

--- src/main.c
+++ src/main.c
@@ -1,35 +1,58 @@
+#define STB_DS_IMPLEMENTATION
+
 #include "connection.h"
-#include "uv.h"
+#include <getopt.h>
 
 int main(int argc, char **argv) {
+  setbuf(stdout, NULL);
 
-  // start a server that others can join
+  int opt;
+  int sport;
+  int dport;
+  char *host = NULL;
+  char *path = NULL;
+
+  struct option flags[] = {
+      {"sport", required_argument, NULL, 's'},
+      {"dhost", optional_argument, NULL, 'h'},
+      {"dport", optional_argument, NULL, 'd'},
+      {"path", optional_argument, NULL, 'p'},
+  };
+
+  while ((opt = getopt_long(argc, argv, "s:h:d:p:", flags, NULL)) != -1) {
+    switch (opt) {
+    case 's':
+      sport = atoi(optarg);
+      break;
+    case 'h':
+      host = optarg;
+      break;
+    case 'd':
+      dport = atoi(optarg);
+      break;
+    case 'p':
+      path = optarg;
+      break;
+    default:
+      fprintf(stderr,
+              "usage: %s -s srcport [-h dsthost] [-d dstport] [-p path]",
+              "pco");
+      exit(EXIT_FAILURE);
+    }
+  }
+
   node_t s;
 
-  switch (argc) {
-  case 3: {
-    char *path = argv[2];
-    server_start(&s, atoi(argv[1]), path);
-    break;
-  }
-
-  // join a node
-  case 4: {
-    int sd = client_connect(argv[2], atoi(argv[3]));
+  if (host != NULL) {
+    // join a node
+    int sd = client_connect(host, dport);
     client_hello(sd, &s);
-  }
-
-  default:
-    perror("invalid command\n");
+  } else {
+    // start a server that others can join
+    server_start(&s, sport, path);
   }
 
   pthread_join(s.thread, NULL);
 
-    // uv_loop_t *loop = uv_default_loop();
-
-    // printf("Default loop.\n");
-    // uv_run(loop, UV_RUN_DEFAULT);
-
-    // uv_loop_close(loop);
-    return 0;
+  return 0;
 }
